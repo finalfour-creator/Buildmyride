74559a1caf7a727993a3dc4be8235f886a5d973e
Abdullah mazhar <abdullahmazhar364@gmail.com>
2026-06-16 03:02:50 +0500

    build error fix

--- src/app/configurator/dashboard/page.jsx
+++ src/app/configurator/dashboard/page.jsx
@@ -1,7 +1,10 @@
+import { Suspense } from "react";
 import DashboardPage from "@/feature/dashboard/DashboardPage";
 
 export default function Page() {
-  return ( 
+  return (
+    <Suspense fallback={null}>
       <DashboardPage />
+    </Suspense>
   );
 }
